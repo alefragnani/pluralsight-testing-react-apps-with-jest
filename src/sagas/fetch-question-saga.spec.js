e46610ad05f69eb9689908d324ade19d7d70b0fa
Alessandro Fragnani <alefragnani@hotmail.com>
2020-08-16 19:59:46 +0000

    Export the function to be mocked (has warning)

---
+++ src/sagas/fetch-question-saga.spec.js
@@ -0,0 +1,9 @@
+import { handleFetchQuestion } from "./fetch-question-saga";
+
+describe('Fetch question saga', () => {
+  it('should fetch the questions', async () => {
+    // console.log('test running');
+    const gen = handleFetchQuestion({question_id: 42});
+    const { value } = gen.next();
+  });
+});

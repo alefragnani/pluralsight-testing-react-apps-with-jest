609c33f5946d613dbe3da8bc500f31b2f7747aef
Alessandro Fragnani <alefragnani@hotmail.com>
2020-08-16 20:02:37 +0000

    Fix the use of the mocked module

--- src/sagas/fetch-question-saga.spec.js
+++ src/sagas/fetch-question-saga.spec.js
@@ -1,10 +1,16 @@
 import { handleFetchQuestion } from "./fetch-question-saga";
+import fetch from "isomorphic-fetch"; // imports the mock version
 
 describe('Fetch question saga', () => {
+  beforeAll(() => {
+    fetch.__setValue([{question_id: 42}]);
+  });
+
   it('should fetch the questions', async () => {
     // console.log('test running');
     const gen = handleFetchQuestion({question_id: 42});
     const { value } = gen.next();
     expect(value).toEqual([{question_id: 42}]);
+    expect(fetch).toHaveBeenCalledWith('/api/questions/42');
   });
 });

7abf7b70974c0c5fe740fa693327e30b009f9205
Alessandro Fragnani <alefragnani@hotmail.com>
2020-08-17 01:39:36 +0000

    Add testing to function (has errors because it was just a simple call)

--- src/components/__tests__/QuestionDetail.jsx
+++ src/components/__tests__/QuestionDetail.jsx
@@ -1,4 +1,17 @@
+import { mapStateToProps } from '../QuestionDetail';
+
 describe('The Question Detail Component', () => {
+
+    describe('The Question Element', () => {
+      describe('mapStateToProps', () => {
+        it('should map the state to props correctly', () => {
+          const appState = {};
+          const ownProps = {};
+          const component = mapStateToProps(appState, ownProps);
+        });
+      });
+    });
+
     it('Should not regress', () => {
         
     })
